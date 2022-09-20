1a4aa285abe20b1bbcc43f8ae84784501891397a
Felipe Marangoni <109756984+FelipeMarangoni@users.noreply.github.com>
2022-09-19 23:30:43 -0300

    Add files via upload

---
+++ nota.js
@@ -0,0 +1,14 @@
+var nota1
+function resultado (params) {
+    if nota1 >=8
+    print ("A")
+    if nota1 >= 7
+    print ("B")
+    if nota1 >= 5
+    print ("C")
+    if nota1 >= 3
+    print("E")
+elif {
+    print ("F")
+}
+}
