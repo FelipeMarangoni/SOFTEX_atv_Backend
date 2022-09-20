65573364a459d0ee45591153ac46ee1d93bafeea
Felipe Marangoni <109756984+FelipeMarangoni@users.noreply.github.com>
2022-09-19 21:30:01 -0300

    Add files via upload

---
+++ Notas estudantes.js
@@ -0,0 +1,19 @@
+console.log ('Notas estudantes')
+var nota1
+var nota2 
+var nota3
+var nota1 = prompt ("Digite a primiera nota")
+var nota2 = prompt ("Digite a segunda nota")
+var nota3 = prompt ("Digite a terceira nota")
+prompt("Digite a primeira nota")
+prompt("Digite a segunda nota")
+prompt("Digite a terceira nota")
+    if{
+        nota1 + nota2 + nota3 >= 7
+        print("Você esta aprovado!!")
+    } 
+    else{
+        nota1 + nota2 + nota3 < 7
+        print("Você esta reprovado")
+    }
+    
